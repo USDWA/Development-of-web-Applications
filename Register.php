4d6815b9b1872e17639e4cccfaf8c3389af540df
5ello <khalil.m.kh@hotmail.com>
2016-12-14 10:33:35 +0400

    new changes
    - New php file called init for initializations to the DB
    - Modifications in the HomePage
    - wrote the php configurations for Register.php

--- Register.php
+++ Register.php
@@ -1,5 +1,33 @@
 <?php
 
-    
+     require "init.php";  // calling init.php (initializing DB)
+
+    $fName = $_POST["fName"];
+    $lName = $_POST["lName"];
+    $SID = $_POST["sid"];
+    $email = $_POST["email"];
+    $phoneNumber = $_POST["phone"];
+
+    //to get the next available guest_id
+    $query = "SELECT max(guest_no) From guests";
+    $result = $connection->query($query);
+    $row = mysqli_fetch_array($result);
+    $guest_id = $row[0] + 1;
+
+    $sql_query = "INSERT INTO guests SET
+                               fNAme = {$fName},
+                               lName = '{$lName}',
+                               ssid = '{$SID}',
+                               email = '{$email}',
+                               phone_no = {$phoneNumber}
+                               guest_no = '{$guest_id}',";
+
+    $result = mysqli_query($connection, $sql_query);
+
+    if(mysqli_num_rows($result))
+        echo "Success...";
+
+    else
+        echo "Failed...";
 
 ?>
---
+++ init.php
@@ -0,0 +1,9 @@
+<?php // Initialize establishing connection_status
+
+    DEFINE('db_name', 'webappdb'); // name of DB
+    DEFINE('mysql_user', 'root'); // user name of admin of DB, our mysql user name is root.
+    DEFINE('mysql_pass', ''); // password of admin of DB
+    DEFINE('server_name', 'localhost'); // server name
+
+    $connection = mysqli_connect(server_name, mysql_user, mysql_pass, db_name); // establishing a connection with the DB
+?>
